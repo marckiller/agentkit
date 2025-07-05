966a421d19f8c66ad16dab0bdee02b217d6c221e
Marcel Półkośnik <marcel.polkosnik@student.uw.edu.pl>
2025-07-05 04:04:05 +0200

    readme update

--- agentkit/base.py
+++ agentkit/base.py
@@ -1,16 +1,15 @@
 class BaseAgent:
-
     def __init__(self, api_url: str, api_key: str, client):
-
-        self.api_url
-        self.api_token
+        self.api_url = api_url
+        self.api_token = api_key
+        self.client = client
 
     def fetch(self) -> dict:
         raise NotImplementedError("You must implement the fetch() method.")
     
     def send(self, data: dict) -> dict:
-        self.client.post("/agent/readings", json={"data": data})
+        return self.client.post("/agent/readings", json={"data": data})
 
     def run(self):
         data = self.fetch()
-        self.send(data)
+        return self.send(data)
